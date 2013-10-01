5fbd2c7fe5fcb5baa8ccb174a0769b23992c25ed
kmcguire <kmcg3413@gmail.com>
2013-10-01 17:25:48 -0500

    [added mutex lock to MDP calls] Made MDP thread safe using a CriticalSection, but for cross-platform these needs to be portable - can be done later.

--- mdp.cpp
+++ mdp.cpp
@@ -1,10 +1,18 @@
 #include "mdp.h"
 #include "linklist.h"
 
+
+CRITICAL_SECTION  mutex;
 MASS_MP_SOCK      *sockchain = 0;
+
+void mass_net_init() {
+   InitializeCriticalSection(&mutex);
+}
 
 int mass_net_sendto(MASS_MP_SOCK *mps, void *buf, uint16 sz, uint32 addr) {
    MASS_MP_PKT          *np;
+
+   EnterCriticalSection(&mutex);
 
    for (MASS_MP_SOCK *cs = sockchain; cs != 0; cs = (MASS_MP_SOCK*)mass_ll_next(cs)) {
       if (addr == 0 || cs->addr == addr) {
@@ -16,20 +24,25 @@ int mass_net_sendto(MASS_MP_SOCK *mps, void *buf, uint16 sz, uint32 addr) {
       }
    }
 
+   LeaveCriticalSection(&mutex);
    return sz;
 }
 
 int mass_net_recvfrom(MASS_MP_SOCK *mps, void *buf, uint16 sz, uint32 *_addr) {
    MASS_MP_PKT       *pkt;
 
+   EnterCriticalSection(&mutex);
+
    pkt = (MASS_MP_PKT*)mass_ll_pop((void**)&mps->in);
 
    if (!pkt) {
+      LeaveCriticalSection(&mutex);
       return 0;
    }
 
    memcpy(buf, pkt->data, pkt->sz > sz ? sz : pkt->sz);
 
+   LeaveCriticalSection(&mutex);
    return pkt->sz > sz ? sz : pkt->sz;
 }
 
@@ -39,8 +52,11 @@ int mass_net_create(MASS_MP_SOCK *mps, uint32 laddr) {
    mps->next = 0;
    mps->prev = 0;
 
+   EnterCriticalSection(&mutex);
+
    mass_ll_add((void**)&sockchain, mps);
 
+   LeaveCriticalSection(&mutex);
    return 1;
 }
 
