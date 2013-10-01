77c3c9ac60ee8de748ff1bc176f4b0b8f87a5078
kmcguire <kmcg3413@gmail.com>
2013-10-01 17:12:42 -0500

    [added new link list methods] added new link list methods addLast and pop to help with new networking support/protocol/...

--- linklist.cpp
+++ linklist.cpp
@@ -7,9 +7,35 @@ void *mass_ll_next(void *ptr) {
    return ((void**)ptr)[0];
 }
 
+void mass_ll_addLast(void **chain, void *toadd) {
+   void           *p;
+   void           *l;
+
+   for (p = *chain, l = *chain; p != 0; l = p, p = mass_ll_next(p));
+
+   if (l == 0) {
+      *chain = toadd;
+   } else {
+      ((void**)p)[0] = toadd;
+   }
+
+   ((void**)toadd)[0] = 0;
+}
+
 void mass_ll_add(void **chain, void *toadd) {
    ((void**)toadd)[0] = *chain;
    *chain = toadd;
+}
+
+void *mass_ll_pop(void **chain) {
+   void           *i;
+
+   i = *chain;
+
+   if (i != 0)
+      mass_ll_rem(chain, i);
+
+   return i;
 }
 
 // potential major performance problem
@@ -25,7 +51,6 @@ void mass_ll_rem(void **chain, void *torem) {
    for (void *ptr = ((void**)*chain)[0], *last = *chain; ptr != 0; last = ptr, ptr = ((void**)ptr)[0]) {
       if (ptr == torem) {
          ((void**)last)[0] = ((void**)ptr)[0];
-         
          return;
       }
    }
--- linklist.h
+++ linklist.h
@@ -7,4 +7,6 @@
 void mass_ll_rem(void **chain, void *torem);
 void mass_ll_add(void **chain, void *toadd);
 void *mass_ll_next(void *ptr);
+void mass_ll_addLast(void **chain, void *toadd);
+void *mass_ll_pop(void **chain);
 #endif
